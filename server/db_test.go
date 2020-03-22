b04bc56f2a5abc3f84b961fe7ed71a1eec704aaa
SnakeHacker <616976756@qq.com>
2020-03-23 01:00:43 +0800

    add test

--- server/db_test.go
+++ server/db_test.go
@@ -12,26 +12,27 @@ func TestValidate(t *testing.T) {
 	dbConf := &DBConf{
 		Database:     "",
 		Username:     "",
-		Password:     "db_password",
+		Password:     "",
 		Port:         1024,
 		Host:         "",
 		Reset:        false,
 		ReconnectSec: time.Second * 3,
 	}
 
-	assert.Error(t, common.ErrDBEmptyHost, dbConf.validate())
-	dbConf.Host = ""
+	assert.Equal(t, common.ErrDBEmptyHost, dbConf.validate())
+	dbConf.Host = "db_host"
 
-	assert.Error(t, common.ErrDBInvalidPort, dbConf.validate())
+	assert.Equal(t, common.ErrDBInvalidPort, dbConf.validate())
 	dbConf.Port = 1025
 
-	assert.Error(t, common.ErrDBEmptyUsername, dbConf.validate())
+	assert.Equal(t, common.ErrDBEmptyUsername, dbConf.validate())
 	dbConf.Username = "db_username"
 
-	assert.Error(t, common.ErrDBEmptyPassword, dbConf.validate())
+	assert.Equal(t, common.ErrDBEmptyPassword, dbConf.validate())
 	dbConf.Password = "db_password"
 
-	assert.Error(t, common.ErrDBEmptyDatabase, dbConf.validate())
-	dbConf.Database = "db_name"
+	assert.Equal(t, common.ErrDBEmptyDatabase, dbConf.validate())
+	// dbConf.Database = "db_name"
 
+	assert.NoError(t, dbConf.validate())
 }
